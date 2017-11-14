55cde57da674f9defdb0058aeda3ff5a3c0fa4d8
Rachel <rachviv@gmail.com>
2017-11-14 00:26:23 +0000

    initial commit

---
+++ src/components/Buttons.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Button from './Button';
+import * as action from '../actions/appActions';
+
+class Buttons extends React.Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            buttons: ['increment', 'decrement']
+        }
+    }
+    
+    render() {
+        return (
+            <div className="app-buttons">
+                {/*<Button onClick={this.props.increment} name="increment" />
+                <Button onClick={this.props.decrement} name="decrement" />*/}
+                {
+                    this.state.buttons.map((btn, i) => {
+                        return (
+                            <Button
+                                key={i}
+                                onClick={this.props[btn]}
+                                name={btn}
+                            />
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = function(state) {
+    return {
+        someProperty: state.someProperty
+    }
+}
+
+const mapDispatchToProps = function(dispatch) {
+    return {
+        increment: () => dispatch(action.increment()),
+        decrement: () => dispatch(action.decrement())
+    }
+}
+
+const ButtonsMap = connect(null, mapDispatchToProps)(Buttons)
+export default ButtonsMap(Buttons);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var htmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new htmlPlugin({
+            template: path.resolve(__dirname, 'src/index.html')
+        })
+    ],
+    devtool: 'cheap-source-map',
+    devServer: {
+        host: '0.0.0.0',
+        contentBase: 'dist',
+        disableHostCheck: true
+    }
+};
